82ac0070df18add3e1714ff7d9fa1cd2a25ea01b
Simon Lalonde <slalonde.design@gmail.com>
2020-03-05 17:20:37 -0500

    Modal Image

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Massively by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -254,5 +248,27 @@
 			});
 
 		}
+
+	// Get the modal
+	var modal = document.getElementById("myModal");
+
+	// Get the image and insert it inside the modal - use its "alt" text as a caption
+	var img = document.getElementById("myImg");
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+	img.onclick = function(){
+  		modal.style.display = "block";
+  		modalImg.src = this.src;
+  		captionText.innerHTML = this.alt;
+	}
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+  	modal.style.display = "none";
+	}
+		
 
 })(jQuery);
